2d1300a3f7209b2d23867015faac099e5be8aeed
Ashim Das <ashim.sp@gmail.com>
2024-02-16 17:25:04 +0530

    docker and k8s setup

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using Shopping.API.Data;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ProductContext _context;
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ProductContext context, ILogger<ProductController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+            => await _context.Products
+                        .Find(p => true)
+                        .ToListAsync();
+    }
+}
